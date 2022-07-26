05d33b69e70219039038fa7db4a73cc0ba7549e9
Pedro Cleto <cl3t0.sh@gmail.com>
2022-07-25 22:45:53 -0300

    feat(nubank-auth): making nubank auth from cert.p12

---
+++ src/custom_request_builder.rs
@@ -0,0 +1,15 @@
+pub trait CustomRequestBuilder {
+    fn apply_default_header(self) -> Self;
+    fn add_authorization(self, access_token: String) -> Self;
+}
+
+impl CustomRequestBuilder for reqwest::RequestBuilder {
+    fn apply_default_header(self) -> Self {
+        self.header("Content-Type", "application/json")
+            .header("X-Correlation-Id", "and-7-86-2-1000005524.9twu3pgr")
+            .header("User-Agent", "MyMoney Client - cl3t0")
+    }
+    fn add_authorization(self, access_token: String) -> Self {
+        self.header("Authorization", format!("Bearer {}", access_token))
+    }
+}

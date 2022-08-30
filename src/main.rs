efec2f9aa62bd251fa13583ea19b6e9cbbdfbdcb
cl3t0 <cl3t0.sh@gmail.com>
2022-08-30 00:55:39 -0300

    feat: creating our hello world graphql api

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
-use tokio;
+use juniper::{EmptyMutation, EmptySubscription};
+use rocket::{response::content, State};
 
-mod nubank;
+#[macro_use]
+extern crate rocket;
 
-mod simple_user_input {
-    use std::io;
-    pub fn get_input(prompt: &str) -> String {
-        println!("{}", prompt);
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_goes_into_input_above) => {}
-            Err(_no_updates_is_fine) => {}
-        }
-        input.trim().to_string()
-    }
+mod graphql_resolvers;
+
+#[get("/")]
+fn graphiql() -> content::RawHtml<String> {
+    juniper_rocket::graphiql_source("/graphql", None)
 }
-use simple_user_input::get_input;
 
-#[tokio::main]
-async fn main() {
-    let cpf = get_input("CPF: ");
-    let password = get_input("Nubank password: ");
+#[rocket::get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: &State<graphql_resolvers::Context>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: &State<graphql_resolvers::Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&*schema, &*context)
+}
 
-    let cert_generator = nubank::NubankClient::new()
-        .request_code_to_gen_cert(&cpf, &password)
-        .await
-        .unwrap();
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: &State<graphql_resolvers::Context>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: &State<graphql_resolvers::Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&*schema, &*context)
+}
 
-    println!("sent to: {:?}", cert_generator.sent_to);
-    let code = get_input("Type the code: ");
-
-    let cert_folder = get_input("Folder to write the certificate: ");
-
-    let cert_path = cert_generator
-        .gen_certificate(&cert_folder, &code)
-        .await
-        .unwrap();
-
-    println!("Certificate generated: {}", &cert_path);
-
-    let nubank_client = nubank::NubankClient::new()
-        .authenticate(&cert_path, &cpf, &password)
-        .await
-        .unwrap();
-    println!("\n\n\n nubank_client = {:?}", nubank_client);
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(graphql_resolvers::Context::new())
+        .manage(graphql_resolvers::Schema::new(
+            graphql_resolvers::Query,
+            EmptyMutation::<graphql_resolvers::Context>::new(),
+            EmptySubscription::<graphql_resolvers::Context>::new(),
+        ))
+        .mount(
+            "/",
+            rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
 }

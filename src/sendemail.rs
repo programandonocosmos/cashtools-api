6dc8ddb6d299d3bbe39c28c65da544ec882288ef
cl3t0 <cl3t0.sh@gmail.com>
2022-09-09 17:12:32 -0300

    fix(sendemail): stop using sendgrid

--- src/sendemail.rs
+++ src/sendemail.rs
@@ -1,32 +1,32 @@
 use reqwest::StatusCode;
-use sendgrid::{Destination, Mail, SGClient};
 
 use dotenvy::dotenv;
 use std::env;
 
 pub enum Error {
-    FailedToSendEmail(sendgrid::SendgridError),
+    FailedToSendEmail(),
     BadStatusCode(StatusCode),
 }
 
 pub fn send_code(email: &str, code: &i32) -> Result<(), Error> {
-    let api_key = env::var("SENDGRID_API_KEY").expect("SENDGRID_API_KEY must be set");
-    let from_email = env::var("EMAIL_FROM").expect("EMAIL_FROM must be set");
-    let email_client = SGClient::new(api_key);
-    let content = format!("Your authentication code is: {}", code);
-    let mail_info = Mail::new()
-        .add_from(&from_email)
-        .add_to(Destination {
-            address: email,
-            name: "My Money",
-        })
-        .add_subject("Your authentication code")
-        .add_content("".to_string(), &content);
-    let response = email_client
-        .send(mail_info)
-        .map_err(Error::FailedToSendEmail)?;
-    match response.status() {
-        StatusCode::OK => Ok(()),
-        s => Err(Error::BadStatusCode(s)),
-    }
+    Ok(())
+    // let api_key = env::var("SENDGRID_API_KEY").expect("SENDGRID_API_KEY must be set");
+    // let from_email = env::var("EMAIL_FROM").expect("EMAIL_FROM must be set");
+    // let email_client = SGClient::new(api_key);
+    // let content = format!("Your authentication code is: {}", code);
+    // let mail_info = Mail::new()
+    //     .add_from(&from_email)
+    //     .add_to(Destination {
+    //         address: email,
+    //         name: "My Money",
+    //     })
+    //     .add_subject("Your authentication code")
+    //     .add_content("".to_string(), &content);
+    // let response = email_client
+    //     .send(mail_info)
+    //     .map_err(Error::FailedToSendEmail)?;
+    // match response.status() {
+    //     StatusCode::OK => Ok(()),
+    //     s => Err(Error::BadStatusCode(s)),
+    // }
 }

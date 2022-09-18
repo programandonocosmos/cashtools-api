9cf68b17b3bcbc595f12ea55976d36d736af658b
cl3t0 <cl3t0.sh@gmail.com>
2022-09-18 18:36:08 -0300

    feat(auth): authorization and authentication first impl done

--- src/services/user.rs
+++ src/services/user.rs
@@ -1,8 +1,8 @@
 use chrono::Utc;
 use rand::Rng;
-use uuid::Uuid;
 
 use crate::database;
+use crate::jwt::{generate_token, verify_token};
 use crate::models::user::{self, NewUser, User};
 use crate::sendemail::send_code;
 
@@ -11,7 +11,7 @@ pub fn create_user(user: NewUser) -> User {
     let now = Utc::now().naive_utc();
     // TODO: Use login_code as a String to generate a code more dificult to crack
     let mut rng = rand::thread_rng();
-    let login_code = rng.gen_range(0..999999);
+    let login_code = rng.gen_range(100000..999999);
     let _ = send_code(&user.email, &login_code);
     let modified_user = NewUser {
         last_code_gen_request: Some(now),
@@ -21,7 +21,19 @@ pub fn create_user(user: NewUser) -> User {
     user::create_user(&mut conn, modified_user)
 }
 
-pub fn delete_user(id: Uuid) -> User {
+pub fn delete_user(token: String) -> User {
+    let email = verify_token(token);
     let mut conn = database::establish_connection();
-    user::delete_user(&mut conn, id)
+    user::delete_user(&mut conn, email)
+}
+
+pub fn validate_and_generate_token(email: String, login_code: i32) -> String {
+    let mut conn = database::establish_connection();
+    let real_login_code = user::get_login_code(&mut conn, &email);
+
+    if login_code == real_login_code {
+        return generate_token(&email);
+    } else {
+        panic!("Login code does not match");
+    }
 }

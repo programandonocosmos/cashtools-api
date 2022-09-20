35db2ba5e144db5e5912d68b045b82e6f725c067
cl3t0 <cl3t0.sh@gmail.com>
2022-09-20 01:40:10 -0300

    feat(all): increasing code quality removing some panics

--- src/services/transaction.rs
+++ src/services/transaction.rs
@@ -1,16 +1,16 @@
 use uuid::Uuid;
 
-use crate::database;
-use crate::models::transaction::{self, Transaction};
+use crate::{
+    database,
+    models::transaction::{self, Transaction},
+};
 
 // TODO: Use the same connection pool through multiple requests
 
-fn create_transaction(t: Transaction) -> Transaction {
-    let mut conn = database::establish_connection();
-    transaction::create_transaction(&mut conn, t)
+fn create_transaction(conn: &database::DbPool, t: Transaction) -> Transaction {
+    transaction::create_transaction(conn, t)
 }
 
-pub fn list_user_transactions(user_id: Uuid) -> Vec<Transaction> {
-    let mut conn = database::establish_connection();
-    transaction::list_user_transactions(&mut conn, user_id)
+pub fn list_user_transactions(conn: &database::DbPool, user_id: Uuid) -> Vec<Transaction> {
+    transaction::list_user_transactions(conn, user_id)
 }
--- src/services/user.rs
+++ src/services/user.rs
@@ -1,13 +1,39 @@
+use std::fmt;
+
 use chrono::Utc;
 use rand::Rng;
 
-use crate::database;
-use crate::jwt::{generate_token, verify_token};
+use hmac::{Hmac, Mac};
+use jwt::{SignWithKey, VerifyWithKey};
+use serde::{Deserialize, Serialize};
+use sha2::Sha256;
+
 use crate::models::user::{self, NewUser, User};
 use crate::sendemail::send_code;
 
-pub fn create_user(user: NewUser) -> User {
-    let mut conn = database::establish_connection();
+use crate::database;
+
+#[derive(Debug)]
+pub enum UserServiceError {
+    UserModelFailed(user::UserModelError),
+    FailedToGenerateJwtKey(sha2::digest::InvalidLength),
+    FailedToGenerateJwtToken(jwt::Error),
+    FailedToVerifyJwtToken(jwt::Error),
+    LoginCodeNotMatching,
+}
+
+impl fmt::Display for UserServiceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct TokenContent {
+    email: String,
+}
+
+pub fn create_user(conn: &database::DbPool, user: NewUser) -> Result<User, UserServiceError> {
     let now = Utc::now().naive_utc();
     // TODO: Use login_code as a String to generate a code more dificult to crack
     let mut rng = rand::thread_rng();
@@ -18,22 +44,50 @@ pub fn create_user(user: NewUser) -> User {
         login_code: Some(login_code),
         ..user
     };
-    user::create_user(&mut conn, modified_user)
+    user::create_user(conn, modified_user).map_err(UserServiceError::UserModelFailed)
 }
 
-pub fn delete_user(token: String) -> User {
-    let email = verify_token(token);
-    let mut conn = database::establish_connection();
-    user::delete_user(&mut conn, email)
+pub fn delete_user(
+    conn: &database::DbPool,
+    token: String,
+    jwt_secret: &str,
+) -> Result<User, UserServiceError> {
+    let email = verify_token(token, jwt_secret)?;
+    user::delete_user(conn, email).map_err(UserServiceError::UserModelFailed)
 }
 
-pub fn validate_and_generate_token(email: String, login_code: i32) -> String {
-    let mut conn = database::establish_connection();
-    let real_login_code = user::get_login_code(&mut conn, &email);
+pub fn validate_and_generate_token(
+    conn: &database::DbPool,
+    email: String,
+    login_code: i32,
+    jwt_secret: &str,
+) -> Result<String, UserServiceError> {
+    let real_login_code =
+        user::get_login_code(conn, &email).map_err(UserServiceError::UserModelFailed)?;
 
     if login_code == real_login_code {
-        return generate_token(&email);
+        return generate_token(&email, jwt_secret);
     } else {
-        panic!("Login code does not match");
+        return Err(UserServiceError::LoginCodeNotMatching);
     }
+}
+
+fn generate_token(email: &str, jwt_secret: &str) -> Result<String, UserServiceError> {
+    let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes())
+        .map_err(UserServiceError::FailedToGenerateJwtKey)?;
+
+    TokenContent {
+        email: email.to_string(),
+    }
+    .sign_with_key(&key)
+    .map_err(UserServiceError::FailedToGenerateJwtToken)
+}
+
+fn verify_token(token: String, jwt_secret: &str) -> Result<String, UserServiceError> {
+    let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes())
+        .map_err(UserServiceError::FailedToGenerateJwtKey)?;
+    let content: TokenContent = token
+        .verify_with_key(&key)
+        .map_err(UserServiceError::FailedToVerifyJwtToken)?;
+    Ok(content.email)
 }

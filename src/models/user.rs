ac7f815242913dfcfa30b666d39c27cb81d4a2d0
cl3t0 <cl3t0.sh@gmail.com>
2022-09-20 01:40:10 -0300

    feat(all): increasing code quality removing some panics

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,8 +1,9 @@
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use r2d2;
 use uuid::Uuid;
 
-use crate::schema::users;
+use crate::{database, schema::users};
 
 #[derive(Queryable, Clone)]
 #[diesel(table_name = users)]
@@ -26,56 +27,70 @@ pub struct NewUser {
     pub login_code: Option<i32>,
 }
 
-pub fn create_user(conn: &mut PgConnection, user: NewUser) -> User {
-    let username_is_available = check_if_username_available(conn, &user.username);
-    let email_is_available = check_if_email_available(conn, &user.email);
+#[derive(Debug)]
+pub enum UserModelError {
+    FailedToGetConn(r2d2::Error),
+    FailedToCreateUser(diesel::result::Error),
+    FailedToDeleteUser(diesel::result::Error),
+    FailedToCheckAvailability(diesel::result::Error),
+    FailedToGetLoginCode(diesel::result::Error),
+    UserAlreadyExists,
+    UserDoesNotExists,
+    UserWithoutLoginCode,
+    MoreThanOneEmailError,
+}
+
+pub fn create_user(conn: &database::DbPool, user: NewUser) -> Result<User, UserModelError> {
+    let username_is_available = check_if_username_available(&conn, &user.username)?;
+    let email_is_available = check_if_email_available(&conn, &user.email)?;
 
     match (username_is_available, email_is_available) {
         (true, true) => diesel::insert_into(users::table)
             .values(&user)
-            .get_result::<User>(conn)
-            .expect("Error saving user"),
-        _ => panic!("User already exists"),
+            .get_result::<User>(&mut conn.get().map_err(UserModelError::FailedToGetConn)?)
+            .map_err(UserModelError::FailedToCreateUser),
+        _ => Err(UserModelError::UserAlreadyExists),
     }
 }
 
-pub fn delete_user(conn: &mut PgConnection, email: String) -> User {
+pub fn delete_user(conn: &database::DbPool, email: String) -> Result<User, UserModelError> {
     diesel::delete(users::table.filter(users::email.eq(email)))
-        .get_result::<User>(conn)
-        .expect("Error deleting user")
+        .get_result::<User>(&mut conn.get().map_err(UserModelError::FailedToGetConn)?)
+        .map_err(UserModelError::FailedToDeleteUser)
 }
 
-fn check_if_username_available(conn: &mut PgConnection, username: &str) -> bool {
-    match users::table
+fn check_if_username_available(
+    conn: &database::DbPool,
+    username: &str,
+) -> Result<bool, UserModelError> {
+    users::table
         .filter(users::username.eq(username))
-        .load::<User>(conn)
-    {
-        Ok(v) => v.is_empty(),
-        Err(_) => panic!("Error checking if username is available"),
-    }
+        .load::<User>(&mut conn.get().map_err(UserModelError::FailedToGetConn)?)
+        .map(|v| v.is_empty())
+        .map_err(UserModelError::FailedToCheckAvailability)
 }
 
-fn check_if_email_available(conn: &mut PgConnection, email: &str) -> bool {
-    match users::table
+fn check_if_email_available(conn: &database::DbPool, email: &str) -> Result<bool, UserModelError> {
+    users::table
         .filter(users::email.eq(email))
-        .load::<User>(conn)
-    {
-        Ok(v) => v.is_empty(),
-        Err(_) => panic!("Error checking if email is available"),
-    }
+        .load::<User>(&mut conn.get().map_err(UserModelError::FailedToGetConn)?)
+        .map(|v| v.is_empty())
+        .map_err(UserModelError::FailedToCheckAvailability)
 }
 
-pub fn get_login_code(conn: &mut PgConnection, email: &str) -> i32 {
+pub fn get_login_code(conn: &database::DbPool, email: &str) -> Result<i32, UserModelError> {
     let result = users::table
         .filter(users::email.eq(email))
-        .load::<User>(conn)
-        .expect("Error getting login code");
+        .load::<User>(&mut conn.get().map_err(UserModelError::FailedToGetConn)?)
+        .map_err(UserModelError::FailedToGetLoginCode)?;
 
     match result.as_slice() {
+        [] => Err(UserModelError::UserDoesNotExists),
         [User {
             login_code: Some(l),
             ..
-        }] => *l,
-        _ => panic!("Error extracting a valid login code"),
+        }] => Ok(l.clone()),
+        [u] => Err(UserModelError::UserWithoutLoginCode),
+        _ => Err(UserModelError::MoreThanOneEmailError),
     }
 }

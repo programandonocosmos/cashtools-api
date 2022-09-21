6136ccd29480022d79e393f9320d39bf1907dc3f
cl3t0 <cl3t0.sh@gmail.com>
2022-09-21 14:38:33 -0300

    tests(users): add tests for jwt token decoding and encoding

--- src/services/user.rs
+++ src/services/user.rs
@@ -58,7 +58,7 @@ pub fn delete_user(
     token: String,
     jwt_secret: &str,
 ) -> Result<User, UserServiceError> {
-    let email = verify_token(token, jwt_secret)?;
+    let email = verify_token(&token, jwt_secret)?;
     Ok(user::delete_user(conn, email)?)
 }
 
@@ -88,11 +88,46 @@ fn generate_token(email: &str, jwt_secret: &str) -> Result<String, UserServiceEr
     .map_err(UserServiceError::FailedToGenerateJwtToken)
 }
 
-fn verify_token(token: String, jwt_secret: &str) -> Result<String, UserServiceError> {
+fn verify_token(token: &str, jwt_secret: &str) -> Result<String, UserServiceError> {
     let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes())
         .map_err(UserServiceError::FailedToGenerateJwtKey)?;
     let content: TokenContent = token
         .verify_with_key(&key)
         .map_err(UserServiceError::FailedToVerifyJwtToken)?;
     Ok(content.email)
+}
+
+#[cfg(test)]
+mod jwt_tests {
+    use super::*;
+
+    const SOME_EMAIL: &str = "someemail@gmail.com";
+    const GOOD_SECRET: &str = "1234567890987654321";
+    const BAD_SECRET: &str = "1234567890987654322";
+    const GOOD_TOKEN: &str = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvbWVlbWFpbEBnbWFpbC5jb20ifQ.8U6HQs0S3UZtbBwUcz2cJwD7d0C6op5QTMsSB1402ys";
+    const BAD_TOKEN: &str = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvbWVlbWFpbEBnbWFpbC5jb20ifQ.8U6HQs0S3UZtbBwUcz2cJwD7d0C6op5QTMsSB1402yt";
+
+    #[test]
+    fn can_generate_token() {
+        let result = generate_token(SOME_EMAIL, GOOD_SECRET).unwrap();
+        assert_eq!(result, GOOD_TOKEN);
+    }
+
+    #[test]
+    fn can_get_email_back() {
+        let result = verify_token(GOOD_TOKEN, GOOD_SECRET).unwrap();
+        assert_eq!(result, SOME_EMAIL)
+    }
+
+    #[test]
+    fn cant_verify_bad_token() {
+        let result = verify_token(BAD_TOKEN, GOOD_SECRET);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn cant_verify_bad_secret() {
+        let result = verify_token(GOOD_TOKEN, BAD_SECRET);
+        assert!(result.is_err());
+    }
 }

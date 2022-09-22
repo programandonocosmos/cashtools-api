bc173bc9f57c5eb240af64fe2dae6dcdb59c747e
cl3t0 <cl3t0.sh@gmail.com>
2022-09-21 21:11:54 -0300

    feat(transactions): remove code coupling between models, services and graphql

--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -2,18 +2,59 @@ use chrono::NaiveDate;
 use diesel::prelude::*;
 use uuid::Uuid;
 
-use crate::{database, schema::transactions};
+use crate::{
+    database, schema::transactions as transaction_schema,
+    services::transaction as transaction_service,
+};
 
-#[derive(Queryable, Insertable, Clone)]
-#[diesel(table_name = transactions)]
-pub struct Transaction {
-    pub id: Uuid,
-    pub related_user: Uuid,
-    pub entry_date: NaiveDate,
-    pub entry_account_code: Option<String>,
-    pub exit_account_code: Option<String>,
-    pub amount: f64,
-    pub description: Option<String>,
+#[derive(Queryable, Clone)]
+#[diesel(table_name = transaction_schema)]
+struct Transaction {
+    id: Uuid,
+    related_user: Uuid,
+    entry_date: NaiveDate,
+    entry_account_code: Option<String>,
+    exit_account_code: Option<String>,
+    amount: f64,
+    description: Option<String>,
+}
+
+#[derive(Insertable, Clone)]
+#[diesel(table_name = transaction_schema)]
+struct NewTransaction {
+    related_user: Uuid,
+    entry_date: NaiveDate,
+    entry_account_code: Option<String>,
+    exit_account_code: Option<String>,
+    amount: f64,
+    description: Option<String>,
+}
+
+impl transaction_service::NewTransaction {
+    fn to_model(&self) -> NewTransaction {
+        NewTransaction {
+            related_user: self.related_user,
+            entry_date: self.entry_date,
+            entry_account_code: self.entry_account_code.clone(),
+            exit_account_code: self.exit_account_code.clone(),
+            amount: self.amount,
+            description: self.description.clone(),
+        }
+    }
+}
+
+impl Transaction {
+    fn to_service(&self) -> transaction_service::Transaction {
+        transaction_service::Transaction {
+            id: self.id,
+            related_user: self.related_user,
+            entry_date: self.entry_date,
+            entry_account_code: self.entry_account_code.clone(),
+            exit_account_code: self.exit_account_code.clone(),
+            amount: self.amount,
+            description: self.description.clone(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -31,20 +72,24 @@ impl From<r2d2::Error> for TransactionModelError {
 
 pub fn create_transaction(
     conn: &database::DbPool,
-    t: Transaction,
-) -> Result<Transaction, TransactionModelError> {
-    diesel::insert_into(transactions::table)
-        .values(&t)
+    t: transaction_service::NewTransaction,
+) -> Result<transaction_service::Transaction, TransactionModelError> {
+    diesel::insert_into(transaction_schema::table)
+        .values(&t.to_model())
         .get_result::<Transaction>(&mut conn.get()?)
+        .map(|t| t.to_service())
         .map_err(TransactionModelError::FailedToCreateTransaction)
 }
 
 pub fn list_user_transactions(
     conn: &database::DbPool,
     user_id: Uuid,
-) -> Result<Vec<Transaction>, TransactionModelError> {
-    transactions::table
-        .filter(transactions::related_user.eq(user_id))
+) -> Result<Vec<transaction_service::Transaction>, TransactionModelError> {
+    Ok(transaction_schema::table
+        .filter(transaction_schema::related_user.eq(user_id))
         .load::<Transaction>(&mut conn.get()?)
-        .map_err(TransactionModelError::FailedToListTransactions)
+        .map_err(TransactionModelError::FailedToListTransactions)?
+        .iter()
+        .map(|t| t.to_service())
+        .collect())
 }

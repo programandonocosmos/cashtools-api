33cb6f68f642af36b56f78d11c0078d4598a76b1
Pedro Cleto <cl3t0.sh@gmail.com>
2023-03-12 20:14:38 -0300

    fix: adjust enum to lowercase

---
+++ migrations/2023-03-12-230005_update_earning_index/down.sql
@@ -0,0 +1,3 @@
+ALTER TYPE earning_index_enum RENAME VALUE 'cdi' TO 'CDI';
+ALTER TYPE earning_index_enum RENAME VALUE 'fixed' TO 'FIXED';
+ALTER TYPE earning_index_enum RENAME VALUE 'ipca' TO 'IPCA';
---
+++ migrations/2023-03-12-230005_update_earning_index/up.sql
@@ -0,0 +1,3 @@
+ALTER TYPE earning_index_enum RENAME VALUE 'CDI' TO 'cdi';
+ALTER TYPE earning_index_enum RENAME VALUE 'FIXED' TO 'fixed';
+ALTER TYPE earning_index_enum RENAME VALUE 'IPCA' TO 'ipca';
